46048b513f6c718e3dd345c628af56b8ff891781
Tim Stadler <timstadler905@googlemail.com>
2015-11-19 17:28:51 +0100

    Initial project commit

---
+++ Midi2KeyPress/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace MidiGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+      this.tabControl1 = new System.Windows.Forms.TabControl();
+      this.tabPage1 = new System.Windows.Forms.TabPage();
+      this.label3 = new System.Windows.Forms.Label();
+      this.comboBox1 = new System.Windows.Forms.ComboBox();
+      this.btnClose = new System.Windows.Forms.Button();
+      this.btnReceiveOut = new System.Windows.Forms.Button();
+      this.listBox2 = new System.Windows.Forms.ListBox();
+      this.label2 = new System.Windows.Forms.Label();
+      this.label1 = new System.Windows.Forms.Label();
+      this.btnRecieve = new System.Windows.Forms.Button();
+      this.textBox1 = new System.Windows.Forms.TextBox();
+      this.listBox1 = new System.Windows.Forms.ListBox();
+      this.tabPage2 = new System.Windows.Forms.TabPage();
+      this.button1 = new System.Windows.Forms.Button();
+      this.btnClearInputListBox = new System.Windows.Forms.Button();
+      this.trackBar1 = new System.Windows.Forms.TrackBar();
+      this.tabControl1.SuspendLayout();
+      this.tabPage1.SuspendLayout();
+      this.tabPage2.SuspendLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // tabControl1
+      // 
+      this.tabControl1.Controls.Add(this.tabPage1);
+      this.tabControl1.Controls.Add(this.tabPage2);
+      this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.tabControl1.Location = new System.Drawing.Point(0, 0);
+      this.tabControl1.Name = "tabControl1";
+      this.tabControl1.SelectedIndex = 0;
+      this.tabControl1.Size = new System.Drawing.Size(436, 357);
+      this.tabControl1.TabIndex = 0;
+      // 
+      // tabPage1
+      // 
+      this.tabPage1.Controls.Add(this.trackBar1);
+      this.tabPage1.Controls.Add(this.btnClearInputListBox);
+      this.tabPage1.Controls.Add(this.label3);
+      this.tabPage1.Controls.Add(this.comboBox1);
+      this.tabPage1.Controls.Add(this.btnClose);
+      this.tabPage1.Controls.Add(this.btnReceiveOut);
+      this.tabPage1.Controls.Add(this.listBox2);
+      this.tabPage1.Controls.Add(this.label2);
+      this.tabPage1.Controls.Add(this.label1);
+      this.tabPage1.Controls.Add(this.btnRecieve);
+      this.tabPage1.Controls.Add(this.textBox1);
+      this.tabPage1.Controls.Add(this.listBox1);
+      this.tabPage1.Location = new System.Drawing.Point(4, 22);
+      this.tabPage1.Name = "tabPage1";
+      this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+      this.tabPage1.Size = new System.Drawing.Size(428, 331);
+      this.tabPage1.TabIndex = 0;
+      this.tabPage1.Text = "Midi In/Out Controller";
+      this.tabPage1.UseVisualStyleBackColor = true;
+      // 
+      // label3
+      // 
+      this.label3.AutoSize = true;
+      this.label3.Location = new System.Drawing.Point(210, 271);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(47, 13);
+      this.label3.TabIndex = 24;
+      this.label3.Text = "Velocity:";
+      // 
+      // comboBox1
+      // 
+      this.comboBox1.FormattingEnabled = true;
+      this.comboBox1.Location = new System.Drawing.Point(213, 227);
+      this.comboBox1.Name = "comboBox1";
+      this.comboBox1.Size = new System.Drawing.Size(91, 21);
+      this.comboBox1.TabIndex = 22;
+      // 
+      // btnClose
+      // 
+      this.btnClose.Enabled = false;
+      this.btnClose.Location = new System.Drawing.Point(113, 140);
+      this.btnClose.Name = "btnClose";
+      this.btnClose.Size = new System.Drawing.Size(91, 23);
+      this.btnClose.TabIndex = 21;
+      this.btnClose.Text = "Close";
+      this.btnClose.UseVisualStyleBackColor = true;
+      this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+      // 
+      // btnReceiveOut
+      // 
+      this.btnReceiveOut.Location = new System.Drawing.Point(310, 227);
+      this.btnReceiveOut.Name = "btnReceiveOut";
+      this.btnReceiveOut.Size = new System.Drawing.Size(99, 23);
+      this.btnReceiveOut.TabIndex = 20;
+      this.btnReceiveOut.Text = "Send Note";
+      this.btnReceiveOut.UseVisualStyleBackColor = true;
+      this.btnReceiveOut.Click += new System.EventHandler(this.btnReceiveOut_Click);
+      // 
+      // listBox2
+      // 
+      this.listBox2.DisplayMember = "Name";
+      this.listBox2.FormattingEnabled = true;
+      this.listBox2.Location = new System.Drawing.Point(8, 227);
+      this.listBox2.Name = "listBox2";
+      this.listBox2.Size = new System.Drawing.Size(196, 95);
+      this.listBox2.TabIndex = 18;
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Location = new System.Drawing.Point(8, 211);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(81, 13);
+      this.label2.TabIndex = 17;
+      this.label2.Text = "Output Devices";
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(8, 5);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(73, 13);
+      this.label1.TabIndex = 16;
+      this.label1.Text = "Input Devices";
+      // 
+      // btnRecieve
+      // 
+      this.btnRecieve.Location = new System.Drawing.Point(8, 140);
+      this.btnRecieve.Name = "btnRecieve";
+      this.btnRecieve.Size = new System.Drawing.Size(99, 23);
+      this.btnRecieve.TabIndex = 15;
+      this.btnRecieve.Text = "Start Receiving";
+      this.btnRecieve.UseVisualStyleBackColor = true;
+      this.btnRecieve.Click += new System.EventHandler(this.btnRecieve_Click);
+      // 
+      // textBox1
+      // 
+      this.textBox1.Location = new System.Drawing.Point(213, 5);
+      this.textBox1.Multiline = true;
+      this.textBox1.Name = "textBox1";
+      this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+      this.textBox1.Size = new System.Drawing.Size(199, 158);
+      this.textBox1.TabIndex = 14;
+      // 
+      // listBox1
+      // 
+      this.listBox1.DisplayMember = "Name";
+      this.listBox1.FormattingEnabled = true;
+      this.listBox1.Location = new System.Drawing.Point(8, 28);
+      this.listBox1.Name = "listBox1";
+      this.listBox1.Size = new System.Drawing.Size(199, 95);
+      this.listBox1.TabIndex = 13;
+      // 
+      // tabPage2
+      // 
+      this.tabPage2.AutoScroll = true;
+      this.tabPage2.Controls.Add(this.button1);
+      this.tabPage2.Location = new System.Drawing.Point(4, 22);
+      this.tabPage2.Name = "tabPage2";
+      this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+      this.tabPage2.Size = new System.Drawing.Size(428, 439);
+      this.tabPage2.TabIndex = 1;
+      this.tabPage2.Text = "Key/Mouse Simulation";
+      this.tabPage2.UseVisualStyleBackColor = true;
+      // 
+      // button1
+      // 
+      this.button1.Location = new System.Drawing.Point(326, 6);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(75, 23);
+      this.button1.TabIndex = 6;
+      this.button1.Text = "Add Rule";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // btnClearInputListBox
+      // 
+      this.btnClearInputListBox.Location = new System.Drawing.Point(337, 169);
+      this.btnClearInputListBox.Name = "btnClearInputListBox";
+      this.btnClearInputListBox.Size = new System.Drawing.Size(75, 23);
+      this.btnClearInputListBox.TabIndex = 25;
+      this.btnClearInputListBox.Text = "Clear";
+      this.btnClearInputListBox.UseVisualStyleBackColor = true;
+      this.btnClearInputListBox.Click += new System.EventHandler(this.btnClearInputListBox_Click);
+      // 
+      // trackBar1
+      // 
+      this.trackBar1.Location = new System.Drawing.Point(281, 256);
+      this.trackBar1.Maximum = 127;
+      this.trackBar1.Name = "trackBar1";
+      this.trackBar1.Size = new System.Drawing.Size(128, 45);
+      this.trackBar1.TabIndex = 26;
+      this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(436, 357);
+      this.Controls.Add(this.tabControl1);
+      this.Name = "Form1";
+      this.Text = "MIDI2KeyPress";
+      this.tabControl1.ResumeLayout(false);
+      this.tabPage1.ResumeLayout(false);
+      this.tabPage1.PerformLayout();
+      this.tabPage2.ResumeLayout(false);
+      ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+      this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnReceiveOut;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnRecieve;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnClearInputListBox;
+        private System.Windows.Forms.TrackBar trackBar1;
+
+    }
+}
+
---
+++ Midi2KeyPress/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using Midi;
+using WindowsInput;
+using System.Runtime.InteropServices;
+using WindowsInput.Native;
+
+namespace MidiGUI
+{
+  public partial class Form1 : Form
+  {
+    [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+    public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+
+    InputDevice inDevice;
+    OutputDevice outDevice;
+
+    InputSimulator simulator = new InputSimulator();
+
+    List<ComboBox> comboBoxNoteList = new List<ComboBox>();
+    List<ComboBox> comboBoxKeyList = new List<ComboBox>();
+    List<CheckBox> checkBoxActiveList = new List<CheckBox>();
+
+    int comboBoxCount = 0;
+
+    public Form1()
+    {
+      this.Icon = Midi2KeyPress.Properties.Resources.logo;
+
+      InitializeComponent();
+      foreach (InputDevice device in InputDevice.InstalledDevices)
+      {
+        listBox1.Items.Add(device);
+      }
+
+      foreach (OutputDevice device in OutputDevice.InstalledDevices)
+      {
+        listBox2.Items.Add(device);
+      }
+
+      fillNoteComboBox(comboBox1);
+
+      label3.Text = "Velocity: " + trackBar1.Value.ToString();
+    }
+
+    private void btnRecieve_Click(object sender, EventArgs e)
+    {
+      if (listBox1.SelectedItem == null)
+      {
+        MessageBox.Show("No device selected!");
+        return;
+      }
+
+      inDevice = (InputDevice)listBox1.SelectedItem;
+
+      try
+      {
+        inDevice.Open();
+        inDevice.NoteOn += new InputDevice.NoteOnHandler(NoteOn);
+        inDevice.StartReceiving(null);  // Note events will be received in another thread
+        btnRecieve.Enabled = false;
+        btnClose.Enabled = true;
+      }
+
+      catch (Midi.DeviceException)
+      {
+        MessageBox.Show("Device is already openend in another program", "Error");
+      }
+    }
+
+    //Is fired when a MIDI note is catched 
+    public void NoteOn(NoteOnMessage msg) 
+    {
+      AppendTextBox(msg.Pitch.ToString());
+
+      for (int i = 0; i < comboBoxCount; i++)
+      {
+        if (checkBoxActiveList[i].Checked)
+        {
+
+          Pitch selectedNote = (Pitch)Invoke(new Func<Pitch>(() => (Pitch)comboBoxNoteList[i].SelectedItem));
+
+          VirtualKeyCode selectedKey = (VirtualKeyCode)Invoke(new Func<VirtualKeyCode>(() => (VirtualKeyCode)comboBoxKeyList[i].SelectedItem));
+
+          if (msg.Pitch == selectedNote)
+          {
+            if (selectedKey == VirtualKeyCode.LBUTTON)
+            {
+              simulator.Mouse.LeftButtonClick();
+            }
+
+            else if (selectedKey == VirtualKeyCode.RBUTTON)
+            {
+              simulator.Mouse.RightButtonClick();
+            }
+
+            else if (selectedKey == VirtualKeyCode.MBUTTON)
+            {
+              simulator.Mouse.XButtonClick(0);
+            }
+
+            simulator.Keyboard.KeyPress(selectedKey);
+          }
+        }
+      }
+    }
+
+    //Write the value of the played note into the textbox
+    public void AppendTextBox(string value)
+    {
+      if (InvokeRequired)
+        this.Invoke(new Action<string>(AppendTextBox), new object[] { value });
+      else
+        textBox1.Text += value + "\r\n";
+    }
+
+    private void btnReceiveOut_Click(object sender, EventArgs e)
+    {
+      if (listBox2.SelectedItem == null)
+        MessageBox.Show("No device selected!");
+      else
+      {
+        outDevice = (OutputDevice)listBox2.SelectedItem;
+        outDevice.Open();
+        if (comboBox1.SelectedItem != null)
+          outDevice.SendNoteOn(Channel.Channel1, (Pitch)comboBox1.SelectedItem, trackBar1.Value);
+
+        outDevice.Close();
+      }
+    }
+
+    private void trackBar1_ValueChanged(object sender, EventArgs e)
+    {
+      label3.Text = "Velocity: " + trackBar1.Value.ToString();
+    }
+
+    private void btnClose_Click(object sender, EventArgs e)
+    {
+      inDevice.StopReceiving();
+      inDevice.Close();
+      btnRecieve.Enabled = true;
+      btnClose.Enabled = false;
+    }
+
+    private void populatePageTwo()
+    {
+      Label noteLabel = new Label();
+      noteLabel.Text = "Note:";
+      noteLabel.Location = new Point(15, 15 * (comboBoxCount + 1) + (20 * comboBoxCount) + 2 /*+2 for being placed into the middle of the combobox */);
+
+      ComboBox noteBox = new ComboBox();
+      noteBox.Location = new Point(50, 15 * (comboBoxCount + 1) + (20 * comboBoxCount));
+      noteBox.Size = new Size(100, 20);
+      noteBox.DropDownStyle = ComboBoxStyle.DropDownList;
+      fillNoteComboBox(noteBox);
+      noteBox.SelectedItem = noteBox.Items[0];
+      comboBoxNoteList.Add(noteBox);
+
+      Label keyLabel = new Label();
+      keyLabel.Text = "Key:";
+      keyLabel.Location = new Point(152, 15 * (comboBoxCount + 1) + (20 * comboBoxCount) + 2);
+
+      ComboBox keyBox = new ComboBox();
+      keyBox.Location = new Point(180, 15 * (comboBoxCount + 1) + (20 * comboBoxCount));
+      keyBox.Size = new Size(100, 20);
+      keyBox.DropDownStyle = ComboBoxStyle.DropDownList;
+      fillKeyComboBox(keyBox);
+      keyBox.SelectedItem = keyBox.Items[0];
+      comboBoxKeyList.Add(keyBox);
+
+      CheckBox activeCheck = new CheckBox();
+      activeCheck.Text = "Active";
+      activeCheck.Location = new Point(285, 15 * (comboBoxCount + 1) + (20 * comboBoxCount));
+      checkBoxActiveList.Add(activeCheck);
+
+      this.tabPage2.Controls.Add(noteBox);
+      this.tabPage2.Controls.Add(keyBox);
+      this.tabPage2.Controls.Add(noteLabel);
+      this.tabPage2.Controls.Add(keyLabel);
+      this.tabPage2.Controls.Add(activeCheck);
+
+      comboBoxCount++;
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      populatePageTwo();
+    }
+
+    private void fillNoteComboBox(ComboBox box)
+    {
+      foreach (Pitch note in Enum.GetValues(typeof(Pitch)))
+      {
+        box.Items.Add(note);
+      }
+    }
+
+    private void fillKeyComboBox(ComboBox box)
+    {
+      foreach (VirtualKeyCode key in Enum.GetValues(typeof(VirtualKeyCode)))
+      {
+        box.Items.Add(key);
+      }
+    }
+
+    private void btnClearInputListBox_Click(object sender, EventArgs e)
+    {
+      textBox1.Clear();
+    }
+  }
+}
